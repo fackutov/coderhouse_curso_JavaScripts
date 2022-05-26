a4a9fd799b269b29d9a1518433af5bfe7d75a755
FacundoTovagliare <fackutov@gmail.com>
2022-05-26 09:43:57 -0300

    funcionabilidad y librerias

--- javascripts/carrito.js
+++
@@ -1,6 +0,0 @@
-
-let botonCompra = document.getElementById('botonCompra')
-
-botonCompra.addEventListener('click', () => {
-    console.log('hola')
-})
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -10,8 +10,6 @@ let btn1 = document.getElementById('btn1')
 let btn2 = document.getElementById('btn2')
 let btn3 = document.getElementById('btn3')
 let btn4 = document.getElementById('btn4')
-let btn5 = document.getElementById('btn5')
-let btn6 = document.getElementById('btn6')
 
 class Producto {
     constructor(id, tipo, nombre, universo, tamanio, precio, stock){
@@ -38,6 +36,8 @@ const prod_9 = new Producto (9, 'juguete', 'cubo Rubik 3x3x3', 'Rubik', '6 x 6 x
 let productos = [prod_1, prod_2, prod_3, prod_4, prod_5, prod_6, prod_7, prod_8, prod_9]
 
 let divProductos = document.getElementById('divProductos')
+let divCarrito = document.getElementById('divCarrito')
+let totalCarrito = document.getElementById('totalCarrito')
 
 productos.forEach(prod => {
     divProductos.innerHTML += `
@@ -47,48 +47,29 @@ productos.forEach(prod => {
             <P class="universoProd">${prod.universo}</P>
             <P class="medidasProd">medidas <br>${prod.tamanio}</P>
             <p class="precioProd">$ ${prod.precio}</p>
-            <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
+            <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
         </div>
     `
 })
 
+btnAll.addEventListener('click', () => {
+    divProductos.innerHTML =``
+
+    productos.forEach(prod => {
+        divProductos.innerHTML += `
+            <div id=producto${prod.id} class="producto">
+                <img src='../multimedia/imagenes/productos/prod${prod.id}.jpg'>
+                <p class="nombreProd">${prod.nombre}</p>
+                <P class="universoProd">${prod.universo}</P>
+                <P class="medidasProd">medidas <br>${prod.tamanio}</P>
+                <p class="precioProd">$ ${prod.precio}</p>
+                <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
+            </div>
+        `
+    })
+})
+
 btn1.addEventListener('click', () => {
-    let productosOrdenados = productos.sort((prod1, prod2) => {
-        return prod1.precio - prod2.precio
-    })
-    divProductos.innerHTML =``
-    productosOrdenados.forEach(prod => {
-        divProductos.innerHTML += `
-            <div id=producto${prod.id} class="producto">
-                <img src='../multimedia/imagenes/productos/prod${prod.id}.jpg'>
-                <p class="nombreProd">${prod.nombre}</p>
-                <P class="universoProd">${prod.universo}</P>
-                <P class="medidasProd">medidas <br>${prod.tamanio}</P>
-                <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
-            </div>
-        `
-})})
-
-btn2.addEventListener('click', () => {
-    let productosOrdenados = productos.sort((prod1, prod2) => {
-        return prod2.precio - prod1.precio
-    })
-    divProductos.innerHTML =``
-    productosOrdenados.forEach(prod => {
-        divProductos.innerHTML += `
-            <div id=producto${prod.id} class="producto">
-                <img src='../multimedia/imagenes/productos/prod${prod.id}.jpg'>
-                <p class="nombreProd">${prod.nombre}</p>
-                <P class="universoProd">${prod.universo}</P>
-                <P class="medidasProd">medidas <br>${prod.tamanio}</P>
-                <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
-            </div>
-        `
-})})
-
-btn3.addEventListener('click', () => {
     let listaProductos = productos.filter(producto => producto.universo == 'Pokemon')
     divProductos.innerHTML =``
 
@@ -100,12 +81,12 @@ btn3.addEventListener('click', () => {
                 <P class="universoProd">${prod.universo}</P>
                 <P class="medidasProd">medidas <br>${prod.tamanio}</P>
                 <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
+                <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
             </div>
         `
 })})
 
-btn4.addEventListener('click', () => {
+btn2.addEventListener('click', () => {
     let listaProductos = productos.filter(producto => producto.universo == 'Marvel')
     divProductos.innerHTML =``
 
@@ -117,13 +98,13 @@ btn4.addEventListener('click', () => {
                 <P class="universoProd">${prod.universo}</P>
                 <P class="medidasProd">medidas <br>${prod.tamanio}</P>
                 <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
+                <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
             </div>
         `
 
 })})
 
-btn5.addEventListener('click', () => {
+btn3.addEventListener('click', () => {
     let listaProductos = productos.filter(producto => producto.universo == 'League of legends')
     divProductos.innerHTML =``
 
@@ -135,12 +116,12 @@ btn5.addEventListener('click', () => {
                 <P class="universoProd">${prod.universo}</P>
                 <P class="medidasProd">medidas <br>${prod.tamanio}</P>
                 <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
+                <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
             </div>
         `
 })})
 
-btn6.addEventListener('click', () => {
+btn4.addEventListener('click', () => {
     let listaProductos = productos.filter(producto => producto.universo == 'Rubik')
     divProductos.innerHTML =``
 
@@ -152,7 +133,7 @@ btn6.addEventListener('click', () => {
                 <P class="universoProd">${prod.universo}</P>
                 <P class="medidasProd">medidas <br>${prod.tamanio}</P>
                 <p class="precioProd">$ ${prod.precio}</p>
-                <button onclick='agregar(${prod.id})' class='botonCompra'>Comprar</button>
+                <button onclick='agregar(${prod.id})' class='botonCompra'>Agregar al carrito</button>
             </div>
         `
 })})
@@ -162,32 +143,169 @@ btn6.addEventListener('click', () => {
 let numCarrito = document.getElementById('numCarrito')
 
 // Agrega el numero al icono de carrito segun la cantidad de elementos que haya en el local storage
-function agregarNum() {
+
+function agregarNum() { //solo agrega el num al icono de carrito
     cantidad = agregarStorage()
     numCarrito.innerHTML = `
     ${cantidad.length}
 `
 }
 
+function totalCompra(){ //suma el precio de todos los productos del carrito
+    let lista = JSON.parse(localStorage.getItem('carrito')) || []
+    let total = 0
 
+    if (lista.lenght != 0)
+        lista.forEach(prod => {
+            total += prod.precio
+    })
+    totalCarrito.innerHTML = `
+        <p>El total de su compra es ${total}</p>
+        <button onclick='comprarTodo()'>Comprar todo</button>
+    `
+}
 
-function agregarStorage (){
+function mostrarCarrito(){  //muestra todos los productos del carrito (queria hacer que esto apareciera en otro HTML pero no pude asi que me lo llevo de tarea)
+    divCarrito.innerHTML =``
+
+    let lista = JSON.parse(localStorage.getItem('carrito')) || []
+
+    if (lista.length != 0 ) {
+        lista.forEach(prod =>{
+            divCarrito.innerHTML += `
+                <div id=producto${prod.id} class="prodCarrito">
+                    <div>
+                        <img class='imgProd' src='../multimedia/imagenes/productos/prod${prod.id}.jpg'>
+                    </div>
+                    <div>
+                        <p class="nombreProd">${prod.nombre}</p>
+                        <P class="universoProd">${prod.universo}</P>
+                        <P class="medidasProd">medidas <br>${prod.tamanio}</P>
+                    </div>
+                    <div>
+                        <p class="precioProd">$ ${prod.precio}</p>
+                    </div>
+                    <div class='btnCompra'>
+                        <button onclick=eliminarProdCarrito(${prod.id})>X</button>
+                        <button onclick='comprar(${prod.id})' class='botonCompra'>Comprar</button>
+                    </div>
+                    
+                </div>
+            ` 
+        })
+    } else {
+        divCarrito.innerHTML = `
+            <div class="prodCarrito">
+                <p>No hay productos en el carrito</p>
+            </div>
+        ` 
+    }
+    totalCompra()
+}
+
+function agregarStorage (){ // recolecta el array de carrito del local storage o en su defecto lo crea
     return JSON.parse(localStorage.getItem('carrito')) || []
 }
 
-function guardarStorage (arraycarrito){
+function guardarStorage (arraycarrito){ 
     localStorage.setItem('carrito', JSON.stringify(arraycarrito))    
 }
 
-function agregar(idprod){
+function agregar(idprod){ //agrego un nuevo producto a la array del carrito y lanzo una pequeña alerta con toastify
     let carrito = agregarStorage()
     let prodEncontrado = productos.find(p => p.id == idprod)
     carrito.push(prodEncontrado)
     guardarStorage(carrito)
-    console.log(carrito)
+    Toastify({
+        text: "agregado correctamente",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
 
     agregarNum(carrito)
+    mostrarCarrito()
+}
+
+function comprar(){ // la funcion de compra solo lanza una pequeña alerta ya que no pasa por todo el tramite de pago
+
+    Swal.fire({
+        icon: 'sucess',
+        title: 'Gracias por su preferencia',
+      })
+}
+
+function comprarTodo(){
+
+    Swal.fire({
+        title: 'Quieres comprar todos los productos?',
+        showDenyButton: true,
+        confirmButtonText: 'si quiero',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.removeItem('carrito')
+          Swal.fire('Gracias por su preferencia', '', 'success')
+          mostrarCarrito()
+          agregarNum()
+          totalCompra()
+        }
+      })
+}
+
+
+function eliminarProdCarrito(idProd){ // elimino un unico producto del carrito al cual le paso el id de ese producto
+
+    let lista = JSON.parse(localStorage.getItem('carrito')) //pido datos del local storage
+    let enumerar = [] //lista donde agregare un numero por cada iteracion del forEach
+
+    lista.forEach(prod => { //itero todos los productos que tengo en el carrito
+        enumerar.push(1) // agrego un numero a la lista enumerar cada vez que pasa por el forEach 
+        let largoenumerar = enumerar.length //coloco esto dentro del ciclo porque quiero saber siempre cuantas veces a pasado por aqui
+
+        if (prod.id == idProd){ //consulto si el id del producto del carrito que esta iterando en ese momento es igual al id del producto que quiero eliminar
+            lista.splice(largoenumerar -1, 1) // elimino el producto que esta en la ultima posicion generada cuando se dio la condicion (si tengo 10 prod en el carrito y quiero eliminar el 5to esta funcion iterara 5 veces y eliminara el producto que se encuentra en la posicion 5 del array que traigo de local storage)
+            Toastify({ //alerta de eliminacion
+                text: "Producto eliminado correctamente",
+                duration: 3000,
+                close: true,
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+              }).showToast();
+        }
+    })
+
+    localStorage.setItem('carrito', JSON.stringify(lista))
+
+    mostrarCarrito() 
+    agregarNum()
+    totalCompra() //reseteo el carrito, el numero en el icono del carrito y el total de la compra
+}
+
+function eliminarCarrito(){ // elimino todos los productos del carrito
+
+    Swal.fire({
+        title: 'Quieres eliminar todos los productos?',
+        showDenyButton: true,
+        confirmButtonText: 'si quiero',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.removeItem('carrito')
+          Swal.fire('Productos eliminados correctamente', '', 'success')
+          mostrarCarrito()
+          agregarNum()
+          totalCompra()
+        }
+      })
 }
 
 agregarNum()
-
+mostrarCarrito()
